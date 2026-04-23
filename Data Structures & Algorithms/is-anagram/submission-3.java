1e0fa3e7286fb46b0c5ff4f17a90e53926e5e8d7
garrycodes55 <156943575+garrycodes55@users.noreply.github.com>
2026-04-23 23:17:13 +0530

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+HashMap<Character,Integer>map = new HashMap<>();
+if(s.length()!=t.length())
+return false;
+for (char i:s.toCharArray()){ 
+map.put(i,map.getOrDefault(i,0)+1);
+    }
+    for(char m:t.toCharArray())
+    {
+        if(map.getOrDefault(m,0)==0)
+        return false;
+        map.put(m,map.get(m)-1);
+        if(map.get(m)<0)
+        return false;
+    }
+for (int i: map.values()){ 
+if(i!=0)
+return false;
+}
+return true;
+}
+}
